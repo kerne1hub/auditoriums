7929aa419114d3eb7307ec9bbe4598f3a7441c11
Kernel <kerstal104@gmail.com>
2020-04-12 13:08:16 +0600

    [+] Init Spring Boot Project

---
+++ backend/src/test/java/com/kernel/auditoriums/AuditoriumsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kernel.auditoriums;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditoriumsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
